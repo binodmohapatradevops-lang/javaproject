d9729e9dc1160476369220c12e67e91dc6d591dd
Punit Parida <punit.parida7008@gmail.com>
2025-09-07 12:08:27 +0530

    ec2-creation

--- main.tf
+++ main.tf
@@ -1,52 +1,12 @@
-# Specify provider
 provider "aws" {
-  region = "us-east-1"
+  region = "us-east-1" # Change to your preferred region
 }
 
-# Create a key pair (optional)
-resource "aws_key_pair" "my_key" {
-  key_name   = "my-key"
-  public_key = file("~/.ssh/id_rsa.pub")
-}
-
-# Create a security group
-resource "aws_security_group" "web_sg" {
-  name        = "web-sg"
-  description = "Allow HTTP and SSH inbound traffic"
-
-  ingress {
-    description = "SSH"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    description = "HTTP"
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-}
-
-# Create an EC2 instance
-resource "aws_instance" "web" {
-  ami           = "ami-0c02fb55956c7d316" # Amazon Linux 2 AMI (example)
+resource "aws_instance" "example" {
+  ami           = "ami-0c94855ba95c71c99" # Amazon Linux 2 AMI (us-east-1)
   instance_type = "t2.micro"
-  key_name      = aws_key_pair.my_key.key_name
-  security_groups = [aws_security_group.web_sg.name]
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "MyEC2Instance"
   }
 }
-
